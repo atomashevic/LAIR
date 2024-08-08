4f490ba6a319ab0f41d8f11e221d295da8ac4668
Aleksandar Tomasevic <atomashevic@gmail.com>
2024-08-08 09:35:55 +0200

    fop update

--- setup.R
+++ setup.R
@@ -1,8 +1,19 @@
-#install.packages("devtools")
-#install.packages("shinyjs", dependencies = TRUE)
-#install.packages("shinydashboard")
+install_if_missing <- function(package) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+}
 
-#devtools::install_local("taipan_0.1.2.tar.gz", force = TRUE, upgrade = "never")
+install_if_missing("devtools")
+install_if_missing("shinyjs")
+install_if_missing("shinydashboard")
+
+if (!require("taipan")) {
+  devtools::install_local("taipan_0.1.2.tar.gz",
+    force = TRUE,
+    upgrade = "never"
+  )
+}
 
 library(taipan)
 library(shiny)
@@ -10,17 +21,10 @@ library(shinydashboard)
 library(dplyr)
 
 generate_unique_name <- function() {
-    paste0("frame_E_", sprintf("%04d", sample(1000:9999, 1)), ".jpg")
-    
+  paste0("frame_E_", sprintf("%04d", sample(1000:9999, 1)), ".jpg")
 }
 
-images <- list.files(path = paste0(getwd(),"/fop/"), pattern = ".jpg", recursive = TRUE)
-
-sample <- images[sample(1:length(images),530)]
-
-save(sample, file ='sample.Rds')
-
-load('sample.Rds')
+load("sample.Rds")
 
 images <- sample
 
@@ -33,21 +37,20 @@ ids <- gsub(".jpg", "", ids)
 data <- data.frame(id = ids, image = images)
 
 generate_unique_name <- function() {
-    paste0("frame_E_", sprintf("%04d", sample(1000:9999, 1)), ".jpg")
-    
+  paste0("frame_E_", sprintf("%04d", sample(1000:9999, 1)), ".jpg")
 }
- 
+
 output_dir <- "fop"
 
 data <- data %>%
-    rowwise() %>%
-    mutate(new_image_path = {
-        old_filepath <- image
-        new_filename <- generate_unique_name()
-        new_filepath <- file.path(output_dir, new_filename)
-        file.copy(old_filepath, new_filepath)
-        new_filepath
-    }) %>%
-    ungroup()
+  rowwise() %>%
+  mutate(new_image_path = {
+    old_filepath <- image
+    new_filename <- generate_unique_name()
+    new_filepath <- file.path(output_dir, new_filename)
+    file.copy(old_filepath, new_filepath)
+    new_filepath
+  }) %>%
+  ungroup()
 
-write.csv(data, 'task.csv', row.names = FALSE)
+write.csv(data, "task.csv", row.names = FALSE)
